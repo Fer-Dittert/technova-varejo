12579e71bc238d8f4e87c4476d569625899812c5
Fer Dittert <dittert.fernanda.s@gmail.com>
2026-05-10 12:24:06 -0300

    Add files via upload

---
+++ glue/amzn-s3-tp2-job.py
@@ -0,0 +1,46 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Amazon S3
+AmazonS3_node1772489742425 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://amzn-s3-tp1/bronze/orders.csv"], "recurse": True}, transformation_ctx="AmazonS3_node1772489742425")
+
+# Script generated for node SQL Query
+SqlQuery2997 = '''
+SELECT *,
+year(created_at) as ano,
+month(created_at) as mes,
+day(created_at) as dia
+FROM vendas
+'''
+SQLQuery_node1772490172359 = sparkSqlQuery(glueContext, query = SqlQuery2997, mapping = {"vendas":AmazonS3_node1772489742425}, transformation_ctx = "SQLQuery_node1772490172359")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1772490172359, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1772489722948", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1772490423037 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1772490172359, connection_type="s3", format="glueparquet", connection_options={"path": "s3://amzn-s3-tp1/prata/", "partitionKeys": ["ano", "mes", "dia"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1772490423037")
+
+job.commit()
